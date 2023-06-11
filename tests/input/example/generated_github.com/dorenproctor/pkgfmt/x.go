0c62f1d0da27cfbebf192eaee737bff346821116
Doren Proctor <dorenproctor@gmail.com>
2023-06-11 00:02:14 -0700

    functioning...

---
+++ tests/input/example/generated_github.com/dorenproctor/pkgfmt/x.go
@@ -0,0 +1,11 @@
+package example
+
+import (
+	"fmt"
+	str "strings"
+)
+
+// Does X things
+func x() {
+	fmt.Println(yzzy(str.Trim("x!", str.ToLower("ASDF"))))
+}
---
+++ tests/input/impt/generated_github.com/dorenproctor/pkgfmt/impt.go
@@ -0,0 +1,7 @@
+package impt
+
+type Impt struct {
+	Alias string
+	Body  string
+	Name  string
+}
