2f6df7ce5c305dde52e267b1a42d899b80663c55
Doren Proctor <dorenproctor@gmail.com>
2023-06-16 00:40:12 -0700

    make OutputGoFile helper func private and move into same file

--- cmd/utils/fileutils/output_go_file.go
+++ cmd/utils/fileutils/output_go_file.go
@@ -1,12 +1,21 @@
 package fileutils
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/dorenproctor/pkgfmt/cmd/impt"
 )
 
 func OutputGoFile(filePath, packageName, body string, imports []impt.Impt) error {
-	s := GetGoFileOutput(packageName, body, imports)
+	s := goFileOutput(packageName, body, imports)
 	return ioutil.WriteFile(filePath, []byte(s), 0644)
+}
+
+func goFileOutput(packageName, body string, imports []impt.Impt) string {
+	body = strings.Trim(body, " \t\n") + "\n"
+	return fmt.Sprintf(`package %s
+%s
+%s`, packageName, impt.GetImportStr(imports), body)
 }
