a74152b92ca2b40ec210ef40d41009d3f36ad589
Doren Proctor <dorenproctor@gmail.com>
2023-06-29 22:21:23 -0700

    change buildDate format in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ set -e
 go build \
     -ldflags " \
     -X 'github.com/dorenproctor/pkgfmt/cmd.version=dev-version' \
-    -X 'github.com/dorenproctor/pkgfmt/cmd.buildDate=$(date)' \
+    -X 'github.com/dorenproctor/pkgfmt/cmd.buildDate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")' \
     " \
     -o bin/pkgfmt
 
