5dbbcec0183f026807e6c3b9e122c2b937f0764a
Doren Proctor <dorenproctor@gmail.com>
2023-06-29 19:21:03 -0700

    hopefully set version vars right

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 set -e
 
-go build -o bin/pkgfmt
+go build \
+    -ldflags " \
+    -X 'github.com/dorenproctor/pkgfmt/cmd.version=dev-version' \
+    -X 'github.com/dorenproctor/pkgfmt/cmd.buildDate=$(date)' \
+    " \
+    -o bin/pkgfmt
+
 bin/pkgfmt "$@"
